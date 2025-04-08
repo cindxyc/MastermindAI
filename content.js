237f20d0307214846b755e62336fde888bc93e27
cindxyc <chencindy.1206@gmail.com>
2025-04-08 16:14:41 -0400

    set up basics

---
+++ content.js
@@ -0,0 +1,2 @@
+// script that runs based on the web page opened
+console.log("Content script loaded");
---
+++ popup.js
@@ -0,0 +1,10 @@
+// For each button, add an event listener to send a message to the content script
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the buttons
+    const searchBtn = document.getElementById('search');
+    const summarizeBtn = document.getElementById('summarize');
+    const checkRelevanceBtn = document.getElementById('checkRelevance');
+    const savePageBtn = document.getElementById('savePage');
+    const viewSavedBtn = document.getElementById('viewSaved');
+});
